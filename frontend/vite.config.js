94d70f957e2820c0e0ed2866adc73548de46b719
RichieRichJr <usagrich@aol.com>
2025-04-18 20:18:20 -0400

    build: Add explicit resolve alias for react-select

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      'react-select': path.resolve(__dirname, 'node_modules/react-select'),
+    },
   }
 }); 
