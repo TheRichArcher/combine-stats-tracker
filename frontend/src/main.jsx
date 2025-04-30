193d3f3fd56d11eecbda394d8c1f2eb0f74c90cf
RichieRichJr <usagrich@aol.com>
2025-04-30 15:47:46 -0400

    feat: Implement password gate for main app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import CoachDashboard from './CoachDashboard';
+import PasswordGate from './PasswordGate';
 import './App.css';
 
 if ('serviceWorker' in navigator) {
@@ -17,7 +18,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route 
+          path="/" 
+          element={
+            <PasswordGate>
+              <App />
+            </PasswordGate>
+          }
+        />
         <Route path="/coaches" element={<CoachDashboard />} />
       </Routes>
     </BrowserRouter>
