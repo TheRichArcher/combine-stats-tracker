f0a899690759f7669816a460bc0a1549adcf2fa1
RichieRichJr <usagrich@aol.com>
2025-05-15 15:39:55 -0400

    Use environment variables for Firebase config and ignore .env file

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTgF4cCRxU0--Fi6nmHCiPqLFrA9aj-AU",
-  authDomain: "woo-combine.firebaseapp.com",
-  projectId: "woo-combine",
-  storageBucket: "woo-combine.appspot.com",
-  messagingSenderId: "94393411053",
-  appId: "1:94393411053:web:9441e7b38c1985e5c8564",
-  measurementId: "G-2M0VP9TQ35"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
